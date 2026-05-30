31445aabec21c4a9071752743c1830fde76005cd
rivki <r0583221748@gmail.com>
2026-05-30 23:51:42 +0300

    feat: implement CategorySelector with dependent dropdown logic

--- LearnFlow AI/frontend/src/App.jsx
+++ LearnFlow AI/frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import RegisterForm from './pages/RegistrationForm';
+import CategorySelector from './components/CategorySelector';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <RegisterForm />
+      {/* <RegisterForm /> */}
+     < CategorySelector/>
     </div>
   )
 }
